6b0b017f8ce2d9e05b64137c51a00f874056b4dc
Abraham Olamilekan Owoyele <owoyeleabraham@gmail.com>
2025-09-28 22:34:03 +0100

    deleted:    node_modules/.bin/acorn

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ function Contact() {
       <h2>Contact Me</h2>
       <p>Email: <a href="mailto:your-owoyeleabraham@gmail.com">your-email@example.com</a></p>
       <p>GitHub: <a href="https://github.com/Spiritual-Dev">github.com/your-username</a></p>
-      <p>LinkedIn: <a href="https://linkedin.com/in/your-profile">linkedin.com/in/your-profile</a></p>
+      <p>LinkedIn: <a href="https://linkedin.com/in/abraham-olamilekan-owoyele">linkedin.com/in/your-profile</a></p>
     </section>
   );
 }
